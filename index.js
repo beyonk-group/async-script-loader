65d99a0b598d3cb1d62d4c8f3c646c0d60a97af2
Antony Jones <antony@beyonk.com>
2020-05-05 11:28:46 +0100

    allow multiple + different tag types

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-export default function (url, test, callback, options = { async: true, defer: true }) {
+export default function (urls, test, callback) {
+  let remaining = urls.length
+
+  function maybeCallback () {
+    remaining = --remaining
+    if (remaining < 1) {
+      callback()
+    }
+  }
+
   if (!test()) {
-    const tag = document.createElement('script')
-    tag.src = url
-    tag.async = options.async
-    tag.defer = options.defer
-    tag.onload = callback
-    document.body.appendChild(tag)
+    urls.forEach(({ type, url, options = { async: true, defer: true }}) => {
+      const isScript = type === 'script'
+      const tag = document.createElement(isScript ? 'script': 'link')
+      if (isScript) {
+        tag.src = url
+        tag.async = options.async
+        tag.defer = options.defer
+      } else {
+        tag.rel = 'stylesheet'
+		    tag.href = url
+      }
+      tag.onload = maybeCallback
+      document.body.appendChild(tag)
+    })
   } else {
     callback()
   }
