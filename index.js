e3c0df37f86c5ee24f8c01849864488da627e686
Antony MacKenzie-Jones <ant@Antonys-MacBook-Pro.local>
2022-11-12 11:54:41 +0000

    allow inline content

--- index.js
+++ index.js
@@ -12,17 +12,27 @@ function load (urls, test, callback) {
     return callback()
   }
 
-  for (const { type, url, options = { async: true, defer: true }} of urls) {
+  for (const { type, url, content, options = { async: true, defer: true }} of urls) {
     const isScript = type === 'script'
     const tag = document.createElement(isScript ? 'script': 'link')
+    const attribute = isScript ? 'src' : 'href'
+    const hasUrl = Boolean(url).valueOf()
+
     if (isScript) {
-      tag.src = url
       tag.async = options.async
       tag.defer = options.defer
     } else {
       tag.rel = 'stylesheet'
-      tag.href = url
     }
+
+    if (hasUrl) {
+      tag[attribute] = url
+    } else {
+      tag.appendChild(
+        document.createTextNode(content)
+      )
+    }
+
     tag.onload = maybeCallback
     document.body.appendChild(tag)
   }
